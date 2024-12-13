ec1b7ad324250e2650fee84c53c5bbdc70ae32ee
srcrapi <>
2024-12-13 16:08:27 +0000

    refactor: removed unused classes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -28,18 +28,18 @@
 	})
 </script>
 
-<main class="h-svh flex flex-col items-center justify-center space-y-5">
+<main class="h-svh flex flex-col items-center justify-center space-y-5 text-white">
 {#if pdfsData}
-	<h1 class="text-5xl font-bold text-white mb-10">PDFs</h1>
+	<h1 class="text-5xl font-bold mb-10">PDFs</h1>
 
 	<input 
 	type="text" 
 	placeholder="Search..." 
 	bind:value={searchQuery}
-	class="bg-zinc-700 rounded-xl p-1.5 text-white"
+	class="bg-zinc-700 rounded-xl p-1.5"
 	>
 
-	<div class="bg-zinc-800 rounded-xl w-[66rem] h-[32rem] p-3 mx-52 overflow-y-auto text-white space-y-2 text-lg">
+	<div class="bg-zinc-800 rounded-xl w-[66rem] h-[32rem] p-3 mx-52 overflow-y-auto space-y-2 text-lg">
 		{#if filteredPdfs.length > 0}
 			{#each filteredPdfs as pdf}
 				<p class="p-1 hover:bg-zinc-900 hover:rounded-md">
@@ -56,6 +56,6 @@
 		{/if}
 	</div>
 {:else}
-	<p class="text-center">Loading...</p>
+	<p class="text-center text-2xl font-bold">Loading...</p>
 {/if}
 </main>
