5786e1ad9d0bf7dbd0528f5f4c349d543f52258c
srcrapi <>
2024-12-13 22:33:47 +0000

    chore: add pdfs site url to cors

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://pdfs-site.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
